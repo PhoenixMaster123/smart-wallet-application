1d0ec489af24e2ba5d42c4e5aff0e14c2df92345
kpopov <kpopov@multamedio.de>
2025-09-26 09:49:33 +0200

    feat: change from record -> class. Change from String -> ModelAndView

--- src/main/java/app/web/controller/IndexController.java
+++ src/main/java/app/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package app.web.controller;
 import app.user.model.User;
 import app.user.property.UserProperties;
 import app.user.service.UserService;
+import app.web.dto.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,8 +40,27 @@ public class IndexController {
     }
 
     @GetMapping("/register")
-    public String getRegisterPage() {
-        return "register";
+    public ModelAndView getRegisterPage() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("register");
+
+        // We send an empty DTO object to the register page because we have there a form that the user will fill in.
+        // We needed it because when we go to the thymeleaf page, the form is empty. If we don't send it, we will get an error.
+        // In the thymeleaf page, we will use this object to bind the form fields to the DTO object.
+        // We use th:object="${registerRequest}" to bind the object to the thymeleaf page.
+        // We fiel the fields of the DTO object to the form fields.
+        // We use th:field="*{username}" to bind the form field to the DTO object. (input field for username)
+
+        // th:action -> specifies the form action URL
+        // Example: th:action="@{/register}" means that the form will be submitted to /register URL.
+        // th:method -> specifies the form method (GET or POST)
+        // Example: th:method="post" means that the form will be submitted using POST
+
+        modelAndView.addObject("registerRequest", new RegisterRequest());
+
+
+        return modelAndView;
     }
 
     @GetMapping("/home")
--- src/main/java/app/web/dto/RegisterRequest.java
+++ src/main/java/app/web/dto/RegisterRequest.java
@@ -4,18 +4,40 @@ import app.user.model.Country;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 // Notes: records are immutable
+//@Builder
+//public record RegisterRequest(
+//    @NotBlank
+//    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
+//    String username,
+//    @NotBlank
+//    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters")
+//    String password,
+//    @NotNull
+//    Country country
+//) {}
+
+// Note: records are with a full argument constructor by default.
+
+@Data
 @Builder
-public record RegisterRequest(
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
     @NotBlank
     @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
-    String username,
+    private String username;
+
     @NotBlank
     @Size(min = 6, max = 6, message = "Password must be exactly 6 characters")
-    String password,
+    private String password;
+
     @NotNull
-    Country country
-) {}
+    private Country country;
+}
