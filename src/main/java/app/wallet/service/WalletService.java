83272424bad4385c504882a4d0c7599cf8154c11
Popov, Kristian <kristian.popov@study.thws.de>
2025-10-04 11:12:14 +0200

    feat: add a withdrawal function and renamed topUp -> deposit

--- src/main/java/app/wallet/service/WalletService.java
+++ src/main/java/app/wallet/service/WalletService.java
@@ -8,6 +8,9 @@ import app.user.model.User;
 import app.wallet.model.Wallet;
 import app.wallet.model.WalletStatus;
 import app.wallet.repository.WalletRepository;
+import app.web.dto.TransferRequest;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,9 +23,12 @@ import java.util.UUID;
 @Service
 public class WalletService {
 
-    private static final String SMART_WALLET_SENDER_IDENTIFIER = "SMART WALLET PLATFORM";
+    private static final String SMART_WALLET_IDENTIFIER = "SMART WALLET PLATFORM";
     private static final String INACTIVE_WALLET_FAILURE_REASON = "Inactive wallet";
+    private static final String INACTIVE_FUNDS_FAILURE_REASON = "Not enough funds";
     private static final String TOP_UP_DESCRIPTION_FORMAT = "Top-up %.2f EUR";
+    private static final String TRANSFER_DESCRIPTION_FORMAT = "Transfer %s <> %s (%.2f EUR)";
+    private static final String WALLET_NOT_OWNED_BY_USER_FAILURE_REASON = "Wallet not owned by user";
 
     private static final BigDecimal INITIAL_WALLET_BALANCE = new BigDecimal("20.00");
     private static final Currency DEFAULT_WALLET_CURRENCY = Currency.getInstance("EUR");
@@ -37,23 +43,72 @@ public class WalletService {
     }
 
     @Transactional
-    public Transaction topUp(UUID walletId, BigDecimal topUpAmount) {
+    public Transaction withdrawal(User user, UUID walletId, BigDecimal amount, String description) {
 
         Wallet wallet = getById(walletId);
-        String transactionDescription = TOP_UP_DESCRIPTION_FORMAT.formatted(topUpAmount.doubleValue());
+        
+        Transaction transaction = Transaction.builder()
+                .owner(user)
+                .sender(wallet.getId().toString())
+                .receiver(SMART_WALLET_IDENTIFIER)
+                .amount(amount)
+                .currency(wallet.getCurrency())
+                .type(TransactionType.WITHDRAWAL)
+                .description(description)
+                .createdOn(LocalDateTime.now())
+                .build();
+
+        if (!isActiveWallet(wallet)) {
+            transaction.setFailureReason(INACTIVE_WALLET_FAILURE_REASON);
+            transaction.setStatus(TransactionStatus.FAILED);
+
+        } else if (!hasSufficientFunds(wallet, amount)) {
+            transaction.setFailureReason(INACTIVE_FUNDS_FAILURE_REASON);
+            transaction.setStatus(TransactionStatus.FAILED);
+        } else if (!isWalletOwnedByUser(user, wallet)) {
+            transaction.setFailureReason(WALLET_NOT_OWNED_BY_USER_FAILURE_REASON);
+            transaction.setStatus(TransactionStatus.FAILED);
+        } else {
+            transaction.setStatus(TransactionStatus.SUCCEEDED);
+            wallet.setBalance(wallet.getBalance().subtract(amount));
+            wallet.setUpdatedOn(LocalDateTime.now());
+            walletRepository.save(wallet);
+        }
+
+        transaction.setBalanceLeft(wallet.getBalance());
+
+        return transactionService.upsert(transaction);
+    }
+
+    public boolean isWalletOwnedByUser(User user, Wallet wallet) {
+        return wallet.getOwner().getId().equals(user.getId());
+    }
+
+    public boolean isActiveWallet(Wallet wallet) {
+        return wallet.getStatus() == WalletStatus.ACTIVE;
+    }
+
+    public boolean hasSufficientFunds(Wallet wallet, BigDecimal amount) {
+        return wallet.getBalance().compareTo(amount) >= 0;
+    }
+
+    @Transactional
+    public Transaction deposit(UUID walletId, BigDecimal topUpAmount, String description) {
+
+        Wallet wallet = getById(walletId);
 
         if(wallet.getStatus() == WalletStatus.INACTIVE) {
 
             return transactionService.createNewTransaction(
                     wallet.getOwner(),
-                    SMART_WALLET_SENDER_IDENTIFIER,
+                    SMART_WALLET_IDENTIFIER,
                     wallet.getId().toString(),
                     topUpAmount,
                     wallet.getBalance(),
                     wallet.getCurrency(),
                     TransactionType.DEPOSIT,
                     TransactionStatus.FAILED,
-                    transactionDescription,
+                    description,
                     INACTIVE_WALLET_FAILURE_REASON
                     );
         }
@@ -64,14 +119,14 @@ public class WalletService {
 
         return transactionService.createNewTransaction(
                 wallet.getOwner(),
-                SMART_WALLET_SENDER_IDENTIFIER,
+                SMART_WALLET_IDENTIFIER,
                 wallet.getId().toString(),
                 topUpAmount,
                 wallet.getBalance(),
                 wallet.getCurrency(),
                 TransactionType.DEPOSIT,
                 TransactionStatus.SUCCEEDED,
-                transactionDescription,
+                description,
                 null
         );
     }
@@ -92,5 +147,30 @@ public class WalletService {
 
     private Wallet getById(UUID walletId) {
         return walletRepository.findById(walletId).orElseThrow(() -> new RuntimeException("Wallet by id " + walletId + " not found"));
+    }
+
+    @Transactional
+    public Transaction transfer(TransferRequest transferRequest) {
+
+        Wallet senderWallet = getById(transferRequest.getWalletId());
+        Wallet receiverWallet = getFirstByUsername(transferRequest.getRecipientUsername());
+
+        String transferDescription = TRANSFER_DESCRIPTION_FORMAT.formatted(senderWallet.getOwner().getUsername(), receiverWallet.getOwner().getUsername(), transferRequest.getAmount());
+        Transaction withdrawalTransaction = withdrawal(senderWallet.getOwner(), senderWallet.getId(), transferRequest.getAmount(), transferDescription);
+
+        if (withdrawalTransaction.getStatus() == TransactionStatus.SUCCEEDED ) {
+            deposit(receiverWallet.getId(), transferRequest.getAmount(), transferDescription);
+        }
+
+        return withdrawalTransaction;
+    }
+
+
+    private Wallet getFirstByUsername(String recipientUsername) {
+
+        return walletRepository.findByOwnerUsername(recipientUsername).stream()
+                .filter(this::isActiveWallet)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Wallet not found for username " + recipientUsername));
     }
 }
